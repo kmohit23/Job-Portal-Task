44450ecc42dd52b5f0d3372ae3183af1c145aaa4
karamohitkumar@gmail.com <karamohitkumar@gmail.com>
2024-11-23 01:17:02 +0530

    feat: add trim and lowercase attributes to user schema fields

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -6,11 +6,14 @@ const userSchema = new Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
