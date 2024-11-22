2f9b961b8529168e2e460e1bb89ea749c7cdc654
karamohitkumar@gmail.com <karamohitkumar@gmail.com>
2024-11-22 14:51:10 +0530

    feat: add user authentication routes for registration and login

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,98 @@
+const User = require("../models/user");
+
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    //Check if user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exits" });
+    }
+
+    //Create new user
+
+    user = new User({
+      name,
+      email,
+      password,
+      role: role || "user",
+    });
+    user.save();
+
+    //Generate JWT token
+    const token = jwt.sign(
+      {
+        userid: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET
+      // { expiresIn: "1h" }
+    );
+
+    res.status(201).json({
+      message: "User registered successfully",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+    //Authenticate Password
+    const passwordValidationResult = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!passwordValidationResult) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    //Generate JWT Token
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET
+      // { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "User Logged In Successfully ",
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+module.exports = { register, login };
--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 
 const connectDB = require("./config/database");
+require("dotenv").config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 //Connect to Database
 connectDB();
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+//Start Server
 app.listen(PORT, () => console.log(`Server running at port : ${PORT}`));
 
 module.exports = app;
