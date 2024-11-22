678f8bfe8fdfe6517a9e91602f9647f134aa159e
karamohitkumar@gmail.com <karamohitkumar@gmail.com>
2024-11-22 14:33:25 +0530

    feat: create user schema

---
+++ Backend/models/user.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
