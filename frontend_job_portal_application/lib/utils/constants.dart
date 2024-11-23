eea7c317212e59d099d97dda42a8b7c519ba9705
karamohitkumar@gmail.com <karamohitkumar@gmail.com>
2024-11-23 13:11:58 +0530

    feat: implement utils and constants

---
+++ frontend_job_portal_application/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class Constants {
+  static const String appName = 'AR Job Portal';
+  static const Locale appLocale = Locale('en', '');
+}
---
+++ frontend_job_portal_application/lib/utils/ui_utils.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class UiUtilities {
+  static FontWeight get semiBold => FontWeight.w600;
+}
