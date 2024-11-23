c6862fed7b88d392cda74a7733a2e48632f70582
karamohitkumar@gmail.com <karamohitkumar@gmail.com>
2024-11-23 13:09:13 +0530

    feat: add OrDivider widget

---
+++ frontend_job_portal_application/lib/widgets/or_text_divider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:frontend_job_portal_application/extensions/extensions_export.dart';
+
+class OrTextDivider extends StatelessWidget {
+  const OrTextDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            endIndent: 20.0,
+            thickness: 1,
+            color: context.primaryDividerColor,
+          ),
+        ),
+        Stack(
+          children: [
+            Text(
+              'or',
+              style: context.labelMedium?.copyWith(
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 2
+                  ..color = context.primaryDividerColor,
+              ),
+            ),
+            Text(
+              'or',
+              style: context.labelMedium
+                  ?.copyWith(color: context.secondaryTextColor),
+            ),
+          ],
+        ),
+        Expanded(
+          child: Divider(
+            indent: 20.0,
+            thickness: 1,
+            color: context.primaryDividerColor,
+          ),
+        ),
+      ],
+    ).paddingSymmetric(vertical: 50);
+  }
+}
